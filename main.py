b4731e078f23b4f4704dfe2c8c8278c89f82c539
Yovo <martinosoyen@gmail.com>
2023-09-19 19:39:27 +0000

    Créer les sous-répertoires suivants dans le répertoire data : cleaned, processed et raw.

--- main.py
+++ main.py
@@ -14,4 +14,8 @@ def create_folder_tree(path):
     #Créer les sous-répertoires suivants : data, docs, models, reports et src.
     for folder in ["data", "docs", "models", "reports", "src"]:
         os.makedirs(os.path.join(path, folder))
+        
+    #Créer les sous-répertoires suivants dans le répertoire data : cleaned, processed et raw.    
+    for sub_folder in ["cleaned", "processed", "raw"]:
+        os.makedirs(os.path.join(path, "data", sub_folder))
 
