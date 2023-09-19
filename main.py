5ebe3a97315845c9c0a5b29a2c18766eb75db415
Yovo <martinosoyen@gmail.com>
2023-09-19 19:38:37 +0000

    Créer le répertoire s'il n'existe pas déjà.

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ def create_folder_tree(path):
     #Vérifier si le répertoire existe déjà. Si c'est le cas, la fonction create_folder_tree() renvoie sans rien faire.
     if os.path.exists(path):
         return
-
+    
+    #Créer le répertoire s'il n'existe pas déjà.
+    os.makedirs(path) 
 
